8bcba4c3629730b526fecb12eaffb95f789fa1d4
Abrar Hussain <abrar.hussain@postnord.com>
2025-03-08 12:26:19 +0100

    Add breaking change BREAKING CHANGE: some ...

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ console.log('Hello from semversionify!');
 console.log('Create new feature');
 console.log('I am a change that doesn\'t trigger release');
 console.log('I am a breaking change.');
+console.log('I am a breaking change. Take 2');
