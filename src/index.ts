1c945de28688ee8d41f5b9fc07a4894c07526681
Abrar Hussain <abrar.hussain@postnord.com>
2025-03-10 08:36:51 +0100

    fix: new fix 1

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ console.log('Create new feature');
 console.log('I am a change that doesn\'t trigger release');
 console.log('I am a breaking change.');
 console.log('I am a breaking change. Take 2');
+console.log('I am a fix');
